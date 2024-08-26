dcc16123e48d795fb57988c285d2c3b2a7bd9229
rafavalentim <rafa.valentim@gmail.com>
2024-08-26 16:05:13 -0300

    Configurando menus.

--- app/src/main/java/com/br/orgs/ui/activity/DetalheProdutoActivity.kt
+++ app/src/main/java/com/br/orgs/ui/activity/DetalheProdutoActivity.kt
@@ -2,11 +2,17 @@ package com.br.orgs.ui.activity
 
 import android.os.Build
 import android.os.Bundle
+import android.util.Log
+import android.view.Menu
+import android.view.MenuItem
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
+import com.br.orgs.R
 import com.br.orgs.databinding.ActivityDetalheProdutoBinding
 import com.br.orgs.extensions.tentaCarregarImagem
 import com.br.orgs.model.DetalheProduto
+
+private const val TAG = "DetalhesProduto"
 
 class DetalheProdutoActivity : AppCompatActivity() {
 
@@ -50,4 +56,28 @@ class DetalheProdutoActivity : AppCompatActivity() {
 
         }
     }
+
+    override fun onCreateOptionsMenu(menu: Menu?): Boolean {
+        menuInflater.inflate(R.menu.menu_detalhes_produto, menu)
+        return super.onCreateOptionsMenu(menu)
+    }
+
+    override fun onOptionsItemSelected(item: MenuItem): Boolean {
+
+        when(item.itemId){
+
+            R.id.menu_detalhe_produto_editar -> {
+                Log.i(TAG, "OnOptionsItemSelected: editar" )
+            }
+
+            R.id.menu_detalhe_produto_remover ->{
+                Log.i(TAG, "OnOptionsItemSelected: remover")
+
+            }
+
+        }
+
+        return super.onOptionsItemSelected(item)
+    }
+
 }
